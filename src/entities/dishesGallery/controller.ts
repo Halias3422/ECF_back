3836d536a7376b7f7c661ac00e4f8d52a09475a1
Halias3422 <v.desaintexupery@gmail.com>
2023-03-04 14:44:10 +0100

    fix(dishesGallery): add static attribute

--- src/entities/dishesGallery/controller.ts
+++ src/entities/dishesGallery/controller.ts
@@ -2,7 +2,7 @@ import { QueryResponse } from '../common/constants';
 import { DishesGalleryQueriesService } from './service.queries';
 
 export class DishesGalleryController {
-  getAllDishesGallery = async (): Promise<QueryResponse> => {
+  static getAllDishesGallery = async (): Promise<QueryResponse> => {
     return await DishesGalleryQueriesService.getAllDishesGallery();
   };
 }
