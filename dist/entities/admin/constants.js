6061e0a24868b609560d1826110ab8e6c0a0844a
Halias3422 <v.desaintexupery@gmail.com>
2023-03-17 21:44:25 +0100

    refactor(*): update main, retry deploy with full app

--- dist/entities/admin/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ADMIN_ROUTES = void 0;
-exports.ADMIN_ROUTES = {
-    getAuthenticatedProtectedUserFromSession: "/api/get-authenticated-protected-user-from-session",
-};
