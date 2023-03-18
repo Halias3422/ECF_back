ebf08d0dfb05a6382b6f9de9863f7678f3e89430
Halias3422 <v.desaintexupery@gmail.com>
2023-03-18 21:32:43 +0100

    test(log): add log to check dish images values

--- app/dist/src/entities/dishes/controller.js
+++ app/dist/src/entities/dishes/controller.js
@@ -90,6 +90,7 @@ DishesController.getAllDishesByCategories = () => __awaiter(void 0, void 0, void
         const retreivedCategories = yield _a.getDishesCategoriesById(retreivedDishes.data);
         if (retreivedCategories.length > 0) {
             const response = yield _a.formatGetDishesByCategoriesResponse(retreivedDishes.data, retreivedCategories);
+            console.log("dishes = " + JSON.stringify(response));
             return {
                 statusCode: 200,
                 data: response,
